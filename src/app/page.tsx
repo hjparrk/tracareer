506ee9985c9e9905bb45cb873a2f79f6e6a4eaa4
Hyojoon Park <hpar9758@naver.com>
2025-03-08 03:25:09 +1100

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,74 @@
 import Image from "next/image";
+import Link from "next/link";
+import heroImg from "../../public/hero-img.png";
+import logoImg from "../../public/logo.svg";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className="relative min-h-screen flex flex-col justify-between">
+      {/* Header */}
+      <header className="sticky top-0 left-0 px-4 sm:px-16 py-5 border-b bg-white z-1 border-black/10 flex items-center justify-between">
+        <Link
+          href={"/"}
+          className="flex space-x-2 items-center text-lg font-extrabold tracking-tighter hover:cursor-pointer"
+        >
+          <div className="h-7 w-7 rounded-sm outline outline-offset-1 outline-black/50">
+            <Image src={logoImg} alt="logo" />
+          </div>
+          <span>TRACAREER</span>
+        </Link>
+        <Link
+          href={"#"}
+          className="text-sm border py-1 px-2 rounded-sm hover:bg-black hover:text-white"
+        >
+          SIGN IN
+        </Link>
+      </header>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+      {/* Content */}
+      <div className="h-full max-w-5xl lg:max-w-6xl mx-auto px-5 py-10 lg:py-20">
+        <div className="mx-auto w-fit flex flex-col space-y-3 text-left">
+          {/* Main */}
+          <div className="grid grid-cols-1 gap-20 lg:gap-5 lg:grid-cols-5">
+            <section className="flex flex-col space-y-10 lg:space-y-20 lg:col-span-3">
+              <div>
+                <div className="font-extrabold tracking-tighter">
+                  <h1 className="text-2xl sm:text-3xl lg:text-4xl">
+                    Lost in Your Job Search?
+                  </h1>
+                  <div className="text-4xl sm:text-6xl lg:text-7xl -rotate-2 flex flex-col space-y-3">
+                    <h1>Track Your</h1>
+                    <h1>Applications with</h1>
+                    <h1 className="w-fit bg-gradient-to-r from-green-300 to-pink-300">
+                      Tracareer !
+                    </h1>
+                  </div>
+                </div>
+
+                <h3 className="text-gray-500/80 font-medium mt-10 text-sm sm:text-base lg:text-lg">
+                  Tracareer is a tool that helps job seekers track their
+                  applications.
+                </h3>
+              </div>
+
+              <Link
+                href={"/"}
+                className="w-fit text-white py-2 px-4 text-sm sm:text-base bg-black rounded hover:bg-gradient-to-r hover:from-green-500 hover:to-pink-500 hover:text-black/80 font-bold"
+              >
+                Create My Tracker
+              </Link>
+            </section>
+
+            <section className="lg:col-span-2">
+              {/* Img */}
+              <Image src={heroImg} alt="" className="object-cover w-full" />
+            </section>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
+      <div className="py-2 bg-gray-50 text-center mt-10">
+        <h1>Copyright © 2025 Harry Park</h1>
+      </div>
     </div>
   );
 }
